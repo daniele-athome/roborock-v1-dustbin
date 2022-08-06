1d4305c52b3e20c848203b4e93178aaced573d92
Daniele Ricci <daniele@casaricci.it>
2022-08-06 11:51:21 +0200

    Now it works nice and checks for everything :)

--- dustbin.sh
+++ dustbin.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # Publish dustbin state by watching a log written by the RobotController
-# WORK IN PROGRESS
+# Dependencies:
+# - mosquitto_pub (package: mosquitto-clients)
+# - jq (package: jq)
 
-# very ugly way to retrieve information from Valetudo (we could probably read this from valetudo_config.json though)
-MQTT_INFO="$(wget -q -O - http://rockrobo/api/v2/valetudo/config/interfaces/mqtt)"
-#MQTT_INFO="$(cat /mnt/data/valetudo_config.json)"
-MQTT_HOST="$(echo "$MQTT_INFO" | grep '"host":'| sed -E 's/.*"host":[ ]*"(.*?)".*/\1/' | cut -d'"' -f1)"
-# FIXME doesn't work by reading from valetudo_config.json (property conflict)
-MQTT_PORT="$(echo "$MQTT_INFO" | grep '"port":'| sed -E 's/.*"port":[ ]*(.*?).*/\1/' | cut -d',' -f1)"
-MQTT_TOPIC="valetudo/$(echo "$MQTT_INFO" | grep '"identifier":'| sed -E 's/.*"identifier":[ ]*"(.*?)".*/\1/' | cut -d'"' -f1)/AttachmentStateAttribute/dustbin"
-
-echo "HOST:<$MQTT_HOST>"
-echo "PORT:<$MQTT_PORT>"
-echo "TOPIC:<$MQTT_TOPIC>"
-
+# configuration data - it SHOULDN'T need to be modified
+VALETUDO_CONFIG=/mnt/data/valetudo_config.json
 WATCH_LOG=/var/run/shm/EVENTTASK_normal.log
 
-# TODO this should probably be a shellfire application... we would have MQTT and JSON modules.
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 1; }
+try() { "$@" || die "cannot $*"; }
+check_for() { which "$@" >/dev/null || die "Unable to locate $*"; }
 
-# TODO json parsing https://github.com/fkalis/bash-json-parser or just sed/awk/grep/whatever
-# TODO mqtt client https://github.com/raphaelcohn/bish-bosh
+check_for mosquitto_pub
+check_for jq
+
+[[ -f "$VALETUDO_CONFIG" ]] || die "Valetudo configuration not found."
+[[ -f "$WATCH_LOG" ]] || die "RobotController log file not found."
+
+MQTT_HOST="$(jq -r .mqtt.connection.host < "$VALETUDO_CONFIG")"
+MQTT_PORT="$(jq -r .mqtt.connection.port < "$VALETUDO_CONFIG")"
+# TODO MQTT TLS settings
+# TODO MQTT authentication data
+MQTT_TOPIC="valetudo/$(jq -r .mqtt.identity.identifier < "$VALETUDO_CONFIG")/AttachmentStateAttribute/dustbin"
 
 tail -0f "$WATCH_LOG" |
   grep --line-buffered -E ".*GetEvent:.*(RE_Mcu_Bin).*" |
@@ -30,7 +34,8 @@ while read -r line; do
     elif [[ "$line" == "BinOut" ]]; then
         PAYLOAD="false"
     else
+        yell "Unexpected string found in log: <$line>"
         continue
     fi
-    mosquitto_pub -d -h "$MQTT_HOST" -r -t "$MQTT_TOPIC" -m "$PAYLOAD"
+    mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -r -t "$MQTT_TOPIC" -m "$PAYLOAD"
 done
